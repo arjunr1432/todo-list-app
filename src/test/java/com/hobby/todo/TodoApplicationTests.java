0eb92f56bfb092d8a3a0bc16ce79f5dec68623b9
Arjun <arjunr1432@gmail.com>
2023-05-22 22:54:13 +0200

    Maven project structure added

---
+++ src/test/java/com/hobby/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobby.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
